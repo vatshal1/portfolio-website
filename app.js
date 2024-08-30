e0becffd9d9469e98f96d21a52bca7fc7c143de1
vatshal <vatshal.negi0727@gmail.com>
2024-08-30 12:25:01 +0530

    updating

---
+++ app.js
@@ -0,0 +1,25 @@
+let menu = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menu.addEventListener("click", () => {
+  menu.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+});
+
+window.addEventListener("scroll", () => {
+  menu.classList.remove("bx-x");
+  navbar.classList.remove("active");
+});
+
+const typed = new Typed(".multiple-text", {
+  strings: [
+    "Frontend Developer",
+    "Backend Developer",
+    "Web Designer",
+    "Tech Enthusiast",
+  ],
+  typeSpeed: 80,
+  backSpeed: 80,
+  backDelay: 1200,
+  loop: true,
+});
